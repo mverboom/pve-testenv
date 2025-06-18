d459f30ab8ae1bf4b1ec7db77c889b7ba5d92298
Mark Verboom <mark@verboom.net>
2025-06-18 09:28:07 +0200

    Added services

--- _pve-testenv.sh
+++ _pve-testenv.sh
@@ -8,6 +8,12 @@ test -e "$basedir/routes-$HOSTNAME" && {
    done < "$basedir/routes-$HOSTNAME"
 }
 test $(ip -br a l | wc -l) -gt 2 && sysctl -q net.ipv4.conf.all.forwarding=1
+
+# Start services
+while read -r line; do
+   eval "$line &"
+done < <( test -e "$basedir/services" && cat "$basedir/services" ; test -e "$basedir/services-$HOSTNAME" && cat "$basedir/services-$HOSTNAME" )
+
 # Configure proxy
 echo 'Acquire::http::Proxy "http://127.0.0.1:40000/";' > /etc/apt/apt.conf.d/01proxy
 echo 'Acquire::https::Proxy "http://127.0.0.1:40000/";' >> /etc/apt/apt.conf.d/01proxy
