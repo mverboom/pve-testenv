f23df21218c8cc457bdaa72174be150fe7362664
Mark Verboom <mark@verboom.net>
2025-07-01 09:24:21 +0200

    Disable sshd, added some comments

--- _pve-testenv.sh
+++ _pve-testenv.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 basedir="/shared"
+
+# Symlink /etc/hosts so no restart is required when it is changed
 test -e "$basedir/hosts" && ln -sf "${basedir}/hosts" /etc/hosts
+
+# No DNS resolving
 rm -f /etc/resolv.conf
+
+# Set routes
 test -e "$basedir/routes-$HOSTNAME" && {
    while read route; do
       ip route add $route
    done < "$basedir/routes-$HOSTNAME"
 }
+
+# Enable forwarding when more than 1 interface
 test $(ip -br a l | wc -l) -gt 2 && sysctl -q net.ipv4.conf.all.forwarding=1
 
+# Disable start of sshd
+systemctl mask ssh.socket
+systemctl disable ssh.service
+systemctl stop ssh.service
+
+# Update neighbours arp cache
 while read if ip; do
    arping -q -w 0 -c 1 -U -I ${if/@*/} ${ip/\/*/}
 done < <( ip -br a l | grep -v "^lo" | awk '{printf "%s %s\n",$1,$3}')
